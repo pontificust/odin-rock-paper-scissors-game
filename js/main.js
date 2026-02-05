2895fc99948336b5493dd7f2043a31f8f83b02df
Pontificust <lihtinmann88@yandex.ru>
2026-02-05 16:57:58 +0300

    Add playRound functions
    Implement a logic of palaying a round of the game

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 /*
 1. Randomly get a number 0, 1, 2;
 2. Map the random number to the corresponding game move (Rock, Paper, or Scissors);
@@ -25,4 +28,54 @@ const getHumanChoice = () => {
     return choices.some((val) => val === humanChoice) ? humanChoice : 'your input is invalid';
 }
 
-console.log(getHumanChoice());
+/*
+1. Get human and computer choices;
+2. Check whose choice is a winning one;
+3. Increment score of the round winner;
+4. Log a string representing the round winner;
+*/
+
+const capitalizeFirstLetter = (str) => {
+    return str.toUpperCase()[0] + str.slice(1);
+}
+
+const showWinner = (winnerMsg, winChoice, loserChoice) => {
+    return `${winnerMsg} ${capitalizeFirstLetter(winChoice)} beats ${capitalizeFirstLetter(loserChoice)}.`;
+}
+
+const checkWinner = (computerChoice, humanChoice, choiceToCompare) => {
+    let winnerIndex = 0;
+    const roundMessages = ['You lose!', 'You win!'];
+    winnerIndex = humanChoice === choiceToCompare ? 1 : 0;
+    if(winnerIndex === 0) {
+        computerScore += 1;
+        return showWinner(roundMessages[winnerIndex], computerChoice, humanChoice);
+    } else {
+        humanScore += 1;
+        return showWinner(roundMessages[winnerIndex], humanChoice, computerChoice);
+    }
+}
+
+const playRound = (computerChoice, humanChoice) => {
+    let roundMessage = '';
+    if(computerChoice === humanChoice) {
+        console.log('The round ended in draw');
+        return;
+    } else{
+        switch(computerChoice) {
+            case 'rock':
+                roundMessage = checkWinner(computerChoice, humanChoice, 'paper');
+                break;
+            case 'paper':
+                roundMessage = checkWinner(computerChoice, humanChoice, 'scissors');
+                break;
+            case 'scissors':
+                roundMessage = checkWinner(computerChoice, humanChoice, 'rock');
+                break;     
+                
+        }
+        console.log(roundMessage);
+    }
+}
+
+playRound(getComputerChoice(), getHumanChoice());
